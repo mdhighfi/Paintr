73a4d72c756bb7ceec4626589e305525d98861a6
mdhighfi <matthew.highfield@gmail.com>
2015-10-13 09:50:33 -0700

    Write application, sessions, users controller and application helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if user
+      log_in!(user)
+      redirect_to posts_url
+    else
+      flash.now[:errors] = ["Bad username / password combo"]
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to new_session_url
+  end
 end
