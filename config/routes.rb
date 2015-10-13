f61026d1da733759e890942d19a3d354f5d9e71c
Matt Highfield <mdhighfi@users.noreply.github.com>
2015-10-13 11:59:41 -0700

    Create photos controller and api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#index'
 
+  namespace :api, defaults: { format: :json } do
+    resources :photos, only: [:create, :destroy, :index, :show, :update]
+  end
+
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
   # resources :photos
