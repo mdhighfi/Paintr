54aa3b44304f1d998b27ff76ad7947e32146a46f
mdhighfi <matthew.highfield@gmail.com>
2015-10-13 10:03:20 -0700

    Write routes, sign in and sign up views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: 'static_pages#index'
+
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+  # resources :photos
+  # resources :tags
 end
