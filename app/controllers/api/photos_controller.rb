17325d09d13435d9835515165f0d2e9ef4047f3f
mdhighfi <matthew.highfield@gmail.com>
2015-10-13 11:59:41 -0700

    Create photos controller and api

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,2 +1,30 @@
 class Api::PhotosController < ApplicationController
+  # def create
+  #   @photo = Photo.new(photo_params)
+  #
+  #   if @photo.save
+  #     render :show
+  #   else
+  #     render json: @photo.errors.full_messages, status: 422
+  #   end
+  # end
+
+  # def destroy
+  #   @photo = Photo.find(params[:id])
+  #   @photo.destroy
+  #   render :show
+  # end
+
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:title, :description, :medium, :surface)
+  end
 end
