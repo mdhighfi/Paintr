1634a5d548b336f0d67d66e809db5482c8e8bf90
mdhighfi <matthew.highfield@gmail.com>
2015-10-13 11:30:37 -0700

    Generate photo model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013162937) do
+ActiveRecord::Schema.define(version: 20151013181957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "title",       null: false
+    t.string   "description"
+    t.string   "medium",      null: false
+    t.string   "surface",     null: false
+    t.integer  "author_id",   null: false
+    t.integer  "album_id",    null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "photos", ["album_id"], name: "index_photos_on_album_id", using: :btree
+  add_index "photos", ["author_id"], name: "index_photos_on_author_id", using: :btree
+  add_index "photos", ["medium"], name: "index_photos_on_medium", using: :btree
+  add_index "photos", ["surface"], name: "index_photos_on_surface", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
