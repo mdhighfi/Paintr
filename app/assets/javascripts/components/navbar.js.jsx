09aecce2c1e7f2cd7f4284e0914a182db111db76
Matt Highfield <mdhighfi@users.noreply.github.com>
2015-10-13 17:04:44 -0700

    Edit navbar, require logging in before viewing static page

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -10,13 +10,13 @@ var Navbar = React.createClass({
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a className="navbar-brand" href="#">Brand</a>
+            <a className="navbar-brand" href="#">Paintr</a>
           </div>
 
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav">
-              <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-              <li><a href="#">Link</a></li>
+              <li className="active"><a href="#">You <span className="sr-only">(current)</span></a></li>
+              <li><a href="#">Explore</a></li>
               <li className="dropdown">
                 <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
                 <ul className="dropdown-menu">
@@ -30,24 +30,17 @@ var Navbar = React.createClass({
                 </ul>
               </li>
             </ul>
-            <form className="navbar-form navbar-left" role="search">
-              <div className="form-group">
-                <input type="text" className="form-control" placeholder="Search"/>
-              </div>
-              <button type="submit" className="btn btn-default">Submit</button>
-            </form>
             <ul className="nav navbar-nav navbar-right">
-              <li><a href="#">Link</a></li>
-              <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                <ul className="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" className="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                </ul>
+              <li>
+                <form className="navbar-form navbar-right" role="search">
+                  <div className="form-group">
+                    <input type="text" className="form-control" placeholder="tag, artist, medium"/>
+                  </div>
+                  <button type="submit" className="btn btn-default">Search</button>
+                </form>
               </li>
+              <li><a href="#">Upload</a></li>
+              <li><a href="#">{window.CURRENT_USERNAME}</a></li>
             </ul>
           </div>
         </div>
