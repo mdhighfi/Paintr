90b2554535f164effc0633b05da172015ccb2def
Matt Highfield <mdhighfi@users.noreply.github.com>
2015-10-14 14:40:26 -0700

    Images show up on the image index page

--- app/assets/javascripts/components/photos/photo_index_item.js.jsx
+++ app/assets/javascripts/components/photos/photo_index_item.js.jsx
@@ -1,13 +1,17 @@
-var PhotoIndexItem = React.createClass({
+window.PhotoIndexItem = React.createClass({
   mixins: [ReactRouter.History],
    showDetail: function () {
      this.history.pushState(null, '/photo/' + this.props.photo.id, {});
    },
 
    render: function () {
+     debugger;
      return(
-       <li onClick={this.showDetail} className="photo-list-item">
-         <p>Title: {this.props.photo.name}</p>
+       <li onClick={this.showDetail} className="photo-index-item">
+         <p><img src={this.props.photo.image_url} width="400" /></p>
+         <p>Title: {this.props.photo.title}</p>
+         <p>Description: {this.props.description}</p>
+         <p><em>{this.props.photo.medium} on {this.props.photo.surface}</em></p>
        </li>
      );
    }
