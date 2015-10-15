42f558d72ed4697ee43cdc067f94fe2f9bcaba75
mdhighfi <matthew.highfield@gmail.com>
2015-10-15 15:00:54 -0700

    Make photo detail size fit the page

--- app/assets/javascripts/components/photo.js.jsx
+++ app/assets/javascripts/components/photo.js.jsx
@@ -1,6 +1,9 @@
 var PhotoDetail = React.createClass({
-  foundPhoto: function(){
+  getInitialState: function() {
+    return { photo: this.foundPhoto() }
+  },
 
+  foundPhoto: function(){
     var found = {};
     PhotoStore.all().forEach(function(photo){
       if (photo.id === parseInt(this.props.params.photoId)) {
@@ -9,13 +12,36 @@ var PhotoDetail = React.createClass({
     }.bind(this));
     return found
   },
+
+  _onChange: function() {
+    this.setState({ photo: this.foundPhoto() })
+  },
+
+  componentDidMount: function () {
+    PhotoStore.addPhotosIndexChangeListener(this._onChange);
+    PhotoStore.addPhotoDetailChangeListener(this._onChange);
+    ApiUtil.fetchAllPhotos();
+  },
+
+  compomentWillUnmount: function () {
+    PhotoStore.removePhotosIndexChangeListener(this._onChange);
+    PhotoStore.removePhotoDetailChangeListener(this._onChange);
+  },
+
   render: function () {
+    var currentPhoto = this.state.photo;
+    var desc = '';
+    if (typeof currentPhoto.description === 'string') {
+      desc = <li>Description: {currentPhoto.description}</li>;
+    }
     return(
       <div className="photo-detail">
-        <p><img src={this.foundPhoto().image_url} width="800" /></p>
-        <p>Title: {this.foundPhoto().title}</p>
-        <p>Description: {this.foundPhoto().description}</p>
-        <p><em>{this.foundPhoto().medium} on {this.foundPhoto().surface}</em></p>
+        <p><img src={this.foundPhoto().image_url} /></p>
+          <ul>
+            <li>Title: {this.state.photo.title}</li>
+            {desc}
+            <li><em>{this.state.photo.medium} on {this.state.photo.surface}</em></li>
+          </ul>
       </div>
     );
   }
