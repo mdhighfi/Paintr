e0280dfcfa4bdbe0166494ed0164c1b0fc4e4de6
mdhighfi <matthew.highfield@gmail.com>
2015-10-15 11:20:42 -0700

    Click on a photo in the index to see the details

--- app/assets/javascripts/components/photo.js.jsx
+++ app/assets/javascripts/components/photo.js.jsx
@@ -1,8 +1,21 @@
-PhotoDetail = React.createClass({
+var PhotoDetail = React.createClass({
+  foundPhoto: function(){
+
+    var found = {};
+    PhotoStore.all().forEach(function(photo){
+      if (photo.id === parseInt(this.props.params.photoId)) {
+        found = photo;
+      }
+    }.bind(this));
+    return found
+  },
   render: function () {
     return(
       <div>
-        {this.props.children}
+        <p><img src={this.foundPhoto().image_url} width="800" /></p>
+        <p>Title: {this.foundPhoto().title}</p>
+        <p>Description: {this.foundPhoto().description}</p>
+        <p><em>{this.foundPhoto().medium} on {this.foundPhoto().surface}</em></p>
       </div>
     );
   }
