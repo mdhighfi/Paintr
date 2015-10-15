4d5105d59a191e0c5e8e2d1097a3ea1a199731ff
mdhighfi <matthew.highfield@gmail.com>
2015-10-15 11:20:42 -0700

    Click on a photo in the index to see the details

--- app/assets/javascripts/components/photos/photo_index_item.js.jsx
+++ app/assets/javascripts/components/photos/photo_index_item.js.jsx
@@ -8,9 +8,6 @@ window.PhotoIndexItem = React.createClass({
      return(
        <li onClick={this.showDetail} className="photo-index-item">
          <p><img src={this.props.photo.image_url} width="400" /></p>
-         <p>Title: {this.props.photo.title}</p>
-         <p>Description: {this.props.description}</p>
-         <p><em>{this.props.photo.medium} on {this.props.photo.surface}</em></p>
        </li>
      );
    }
