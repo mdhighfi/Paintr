ab3eea3fd6836855ad1a3e9f1d69106ceec03fa4
mdhighfi <matthew.highfield@gmail.com>
2015-10-14 17:18:14 -0700

    Give uploader uploading capability

--- app/assets/javascripts/components/index.js.jsx
+++ app/assets/javascripts/components/index.js.jsx
@@ -2,7 +2,7 @@ window.App = React.createClass({
   render: function () {
     return(
       <div>
-        <div>
+        <div className="extra-margin-bottom">
           <Navbar/>
         </div>
         {this.props.children}
