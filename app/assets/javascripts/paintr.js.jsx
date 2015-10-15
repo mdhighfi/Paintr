a89ae99bfafb4be8878cb0d58ddac6d902b5ee89
mdhighfi <matthew.highfield@gmail.com>
2015-10-15 10:47:45 -0700

    Refactor navbar

--- app/assets/javascripts/paintr.js.jsx
+++ app/assets/javascripts/paintr.js.jsx
@@ -6,10 +6,11 @@ $(function () {
   var rootEl = document.getElementById('content');
   var routes = (
     <Route path="/" component={App}>
-      <IndexRoute component={PhotosIndex} />
+      <IndexRoute component={PhotosIndex}>
+      </IndexRoute>
       <Route path="photos/:photoId" component={PhotoDetail} />
     </Route>
   );
-
+// PhotosIndex
   React.render(<Router>{routes}</Router>, rootEl);
 });
