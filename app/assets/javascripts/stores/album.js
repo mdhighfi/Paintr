7738e1689d77234d6c1cf05459d45466d01f00b3
mdhighfi <matthew.highfield@gmail.com>
2015-10-22 15:44:45 -0700

    Create album store

---
+++ app/assets/javascripts/stores/album.js
@@ -0,0 +1,94 @@
+(function () {
+  var ALBUMS_INDEX_CHANGE_EVENT = "albumsIndexChange";
+  var ALBUM_DETAIL_CHANGE_EVENT = "albumDetailChange";
+  var ALBUM_DELETE_CHANGE_EVENT = "albumDeleteChange";
+
+  var _albums = [];
+
+  var resetAlbums = function (albums) {
+    _albums = albums;
+  };
+
+  var resetAlbum = function (album) {
+    var changed = false;
+    _albums.forEach(function (p) {
+      if(p.id === album.id) {
+        _albums[_albums.indexOf(p)] = album;
+        changed = true;
+      }
+    });
+    if(!changed) { _albums.push(album); }
+  };
+
+  var deleteAlbum = function (album) {
+    _albums.forEach(function (p) {
+      if(p.id === album.id) {
+        _albums.splice(_albums.indexOf(p), 1);
+      }
+    });
+  };
+
+  var editAlbum = function (album) {
+    _albums.forEach(function (p) {
+      if(p.id === album.id) {
+        _albums[_albums.indexOf(p)] = album;
+      }
+    });
+  };
+
+  window.AlbumStore = $.extend({}, EventEmitter.prototype, {
+    all: function () {
+      return _albums.slice();
+    },
+
+    find: function (id) {
+      var album;
+      _albums.forEach(function(p) {
+        if(p.id === id) { album = p; }
+      })
+
+      return album;
+    },
+
+    addAlbumDetailChangeListener: function (callback) {
+      this.on(ALBUM_DETAIL_CHANGE_EVENT, callback);
+    },
+
+    removeAlbumDetailChangeListener: function (callback) {
+      this.removeListener(ALBUM_DETAIL_CHANGE_EVENT, callback);
+    },
+
+    addAlbumDeleteChangeListener: function (callback) {
+      this.on(ALBUM_DELETE_CHANGE_EVENT, callback);
+    },
+
+    removeAlbumDeleteChangeListener: function (callback) {
+      this.removeListener(ALBUM_DELETE_CHANGE_EVENT, callback);
+    },
+
+    addAlbumsIndexChangeListener: function (callback) {
+      this.on(ALBUMS_INDEX_CHANGE_EVENT, callback);
+    },
+
+    removeAlbumsIndexChangeListener: function (callback) {
+      this.removeListener(ALBUMS_INDEX_CHANGE_EVENT, callback);
+    },
+
+    dispatcherID: AppDispatcher.register(function (payload) {
+      switch(payload.actionType) {
+        case AlbumConstants.ALBUMS_RECEIVED:
+          resetAlbums(payload.albums);
+          AlbumStore.emit(ALBUMS_INDEX_CHANGE_EVENT);
+          break;
+        case AlbumConstants.ALBUM_RECEIVED:
+          resetAlbum(payload.album);
+          AlbumStore.emit(ALBUM_DETAIL_CHANGE_EVENT);
+          break;
+        case AlbumConstants.ALBUM_DELETED:
+          deleteAlbum(payload.album);
+          AlbumStore.emit(ALBUM_DELETE_CHANGE_EVENT);
+          break;
+      }
+    })
+  });
+ })();
