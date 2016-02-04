5a27be901404deec1bb7eaf36d15aa6b8334514e
Matt Highfield <mdhighfi@users.noreply.github.com>
2016-02-04 11:22:46 -0800

    Fix cloudinary info

--- app/assets/javascripts/components/photos/photo_index_item.js.jsx
+++ app/assets/javascripts/components/photos/photo_index_item.js.jsx
@@ -1,23 +1,23 @@
 window.PhotoIndexItem = React.createClass({
   mixins: [ReactRouter.History],
-   showDetail: function () {
-     this.history.pushState(null, '/photo/' + this.props.photo.id, {});
-   },
+  showDetail: function () {
+    this.history.pushState(null, '/photo/' + this.props.photo.id, {});
+  },
 
-   _manipulateImage: function(url) {
-     var regexp = /\/upload\//;
-     var manipulation = "h_250\/";
-     return url.replace(regexp, "$&" + manipulation);
-   },
+  _manipulateImage: function(url) {
+    var regexp = /\/upload\//;
+    var manipulation = "h_250\/";
+    return url.replace(regexp, "$&" + manipulation);
+  },
 
-   render: function () {
-     var indexItemImageUrl = this._manipulateImage(this.props.photo.image_url);
-     return(
-       <li onClick={this.showDetail} className="photo-index-item">
-         <img
-           src={indexItemImageUrl}
-         />
-        </li>
-     );
-   }
+  render: function () {
+    var indexItemImageUrl = this._manipulateImage(this.props.photo.image_url);
+    return(
+      <li onClick={this.showDetail} className="photo-index-item">
+        <img
+          src={indexItemImageUrl}
+        />
+      </li>
+    );
+  }
 });
